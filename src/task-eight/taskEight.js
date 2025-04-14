d11f32655893f8a927828dd384f25ab4bba978c1
Иван Маковский <146020410+erexon@users.noreply.github.com>
2025-04-14 11:08:35 +0300

    task eight upd

--- src/task-eight/taskEight.js
+++ src/task-eight/taskEight.js
@@ -1,18 +1,35 @@
+const progressWrapper = document.createElement('div');
+progressWrapper.id = 'progressWrapper';
+progressWrapper.style.width = '100%';
+progressWrapper.style.backgroundColor = '#ccc';
+progressWrapper.style.height = '30px';
+document.body.appendChild(progressWrapper);
+
+const progressBar = document.createElement('div');
+progressBar.id = 'progressBar';
+progressBar.style.width = '0%';
+progressBar.style.backgroundColor = '#4caf50';
+progressBar.style.height = '100%';
+progressWrapper.appendChild(progressBar);
+
+const statusText = document.createElement("p");
+statusText.textContent = 'Progress: ';
+document.body.appendChild(statusText);
+
 function startLoading() {
     let progress = 0;
-    const progressBar = document.getElementById('progressBar');
-    const statusText = document.getElementById('statusText');
-
     const interval = setInterval(() => {
         progress++;
-
         progressBar.style.width = progress + '%';
         statusText.textContent = 'progress: ' + progress + '%';
-
         if (progress >= 100) {
             clearInterval(interval);
         }
     }, 100);
 }
+const startButton = document.createElement("button");
+startButton.id = 'startButton';
+startButton.textContent = "Start Loadong";
+document.body.appendChild(startButton);
 
 document.getElementById('startButton').addEventListener('click', startLoading);
